6931949973988716dba67366b5a4b5f5fad1e344
jak <jak@trilixium.com>
2020-07-26 15:00:58 -0400

    Assignment 5
    Completed all 3 parts of Assignment 1.5

---
+++ Topic1-5c-Final/final-demo.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once 'Person.php';
+require_once 'Student.php';
+
+$newGuy1 = new Person;
+$newGuy2 = new Student;
+
+$newGuy1->growOlderBy(2);
+$newGuy2->growOlderBy(2);
+
+echo "The age of person 1 " . $newGuy1->age . "<br />";
+echo "The age of person 2 " . $newGuy2->age . "<br />";
+
+
+
+?>
